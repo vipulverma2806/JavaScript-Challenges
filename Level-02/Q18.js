d583a2850d04c2ee35a08d41c08e47aba12a9317
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2026-01-14 00:49:12 +0530

    Q18

--- Level-02/Q18.js
+++ Level-02/Q18.js
@@ -9,3 +9,29 @@
 //   * Else if all sides equal → Equilateral
 //   * Else if any two equal → Isosceles
 //   * Else → Scalene
+
+function checkTriangleType(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return "Only Positive value Allowed";
+  }
+  const sorted = [a, b, c].sort((a, b) => a - b);
+  const istrianglePossible = sorted[0] + sorted[1] > sorted[2];
+  if (!istrianglePossible) {
+    return "Triangle not possible ";
+  }
+  const isRightAngled = sorted[0] ** 2 + sorted[1] ** 2 === sorted[2] ** 2;
+  const isIsosceles = a === b || b === c || c === a;
+  if (isRightAngled && isIsosceles) {
+    return "It is Right Angled Isosceles triangle ";
+  } else if (isRightAngled) {
+    return "It is Right Angled triangle";
+  } else if (a === b && b === c) {
+    return "Equilateral";
+  } else if (isIsosceles) {
+    return "Isosceles";
+  } else {
+    return "Scalene";
+  }
+}
+
+console.log(checkTriangleType(2, 3, 3));
